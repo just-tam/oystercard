76435aa3f5aeb07246bb2d684047558ae198bed7
Tamara Jeffrey <just-tam@live.com>
2020-08-10 17:12:42 +0100

    Wrote tests to return a balance of zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,13 @@ require 'Oystercard'
 
 describe Oystercard do
 
+  it "Expect oystercard to respond to balance method" do
+    expect(subject).to respond_to(:balance)
+  end
+
+  it "Expects balance to be zero" do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq (0)
+  end
+
 end
