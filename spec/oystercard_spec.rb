50474e03095906abeda783ab69c0059ee839722c
Tamara Jeffrey <just-tam@live.com>
2020-08-10 18:28:00 +0100

    Added ability to top up oyster card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,19 @@ describe Oystercard do
     expect(oystercard.balance).to eq (0)
   end
 
+  it "Expect oystercard to respond to top_up method" do
+    expect(subject).to respond_to(:top_up)
+  end
+
+  it "Expect oystercard to respond to top_up method with an amount" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  describe "#top_up" do
+    it "Expects balance to increase by top up amount" do
+      balance = subject.balance
+      amount = 10
+      expect(subject.top_up(amount)).to eq(balance + amount)
+    end
+  end
 end
